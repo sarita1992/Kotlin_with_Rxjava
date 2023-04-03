7aa3b26c0bdd5d2f4fb8d1968e7e212d0f99e19b
sarita1992 <1992saritacjhaudhary22gmail.com>
2023-04-03 08:11:33 -0800

    coding test

---
+++ app/src/main/java/com/example/test/networkCall/NetworkService.kt
@@ -0,0 +1,25 @@
+package com.example.test
+
+import com.example.test.networkCall.BasicApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+private fun httpInterceptor() = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+
+private fun basicOkHttpClient() = OkHttpClient.Builder().addInterceptor(httpInterceptor()).build()
+
+
+fun createBasicAuthService(): BasicApiService {
+    val retrofit = Retrofit.Builder()
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create()).client(basicOkHttpClient())
+        .baseUrl("https://randomuser.me/")
+        .build()
+    return retrofit.create(BasicApiService::class.java);
+}
